b2508e71e186db5a6c41a6b70a34afe49fc31b2d
Alex Day <alexanderday@email.arizona.edu>
2021-01-21 21:16:39 -0700

    Implement templates and static files for Django

--- ProTwo/AppTwo/views.py
+++ ProTwo/AppTwo/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<em>My Second App</em>')
+    my_dict = {
+        'insert_me' : 'Hello I am from the views.py file!'
+    }
+    return render(request, 'AppTwo/index.html', context=my_dict)
+
+def help(request):
+    temp_dict = {
+        'helpText' : 'This is the help page!'
+    }
+    return render(request, 'AppTwo/help.html', context=temp_dict)
